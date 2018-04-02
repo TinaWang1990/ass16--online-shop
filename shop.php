df8c5f0a45cc3ad0497b787c398bf54e45da0f58
TinaWang <shihuiwang1990@gmail.com>
2018-04-02 14:47:17 +0000

    shop--add price

--- shop.php
+++ shop.php
@@ -18,16 +18,7 @@ require_once('db.php');
 
     <title>Tina's Shop</title>
 
-    <script type="text/javascript">
-      $(document).ready(function(){
-        $('#btn').click(function(){
-          var i=count++;
-
-
-
-        });
-      });
-    </script>
+    
 
   </head>
   <body>
@@ -62,7 +53,9 @@ require_once('db.php');
                 </b>
               </p>
               <div class="text-right">
-                <button class="btn btn-success">
+                <button class="btn btn-success" onclick="AddPrice('<?php 
+                    echo $val['price'];
+                   ?>')">
                   Purchase
                 </button>
               </div>
@@ -79,20 +72,23 @@ require_once('db.php');
 </div>
 
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-  	
+ 	
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+      function AddPrice(price){
+        var current_price=$('.TotalPrice').text();
+        $('.TotalPrice').text((parseFloat(current_price)+parseFloat(price)).toFixed(2));
+      }
+    </script>
+
+
+
+
   </body>
 </html>
